91633d263fe1edb27ac4d58228f0f290ffd70232
AJ-Romera <ajromeraalfonso@gmail.com>
2021-01-26 16:49:38 +0100

    CHNG: sidebar__search JS and CSS

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,6 +5,7 @@ import './Sidebar.css';
 import DonutLargeIcon from '@material-ui/icons/DonutLarge';
 import ChatIcon from '@material-ui/icons/Chat';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
 import { Avatar, IconButton } from '@material-ui/core';
 
 function Sidebar() {
@@ -22,6 +23,13 @@ function Sidebar() {
                     <IconButton>
                         <MoreVertIcon />
                     </IconButton>
+                </div>
+            </div>
+
+            <div className='sidebar__search'>
+                <div className='sidebar__searchContainer'>
+                    <SearchOutlinedIcon />
+                    <input placeholder='Search or start new chat' type='text' />
                 </div>
             </div>
         </div>
