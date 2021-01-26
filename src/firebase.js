baa2f89e0cf4e2ca1207465b174bb92efe896bc8
AJ-Romera <ajromeraalfonso@gmail.com>
2021-01-26 12:56:45 +0100

    START: Create React App and Cleanup npx create-react-app Cleanup unnecessary files, code and styles Add firebase config to firebase.js Update .gitignore

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: 'AIzaSyDWj7oyXnRySKpNsCBq0taI74wmRtDU_Eo',
+    authDomain: 'chautapp.firebaseapp.com',
+    projectId: 'chautapp',
+    storageBucket: 'chautapp.appspot.com',
+    messagingSenderId: '843210553285',
+    appId: '1:843210553285:web:151183bd2b18dc6b3bfdac',
+};
