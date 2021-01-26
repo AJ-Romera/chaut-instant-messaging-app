cd7844afbdee3e3e6866970459f51cbdd8b8a992
AJ-Romera <ajromeraalfonso@gmail.com>
2021-01-26 13:14:20 +0100

    CHNG: Create Components Folder and Sidebar Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Sidebar from './components/Sidebar';
 
 function App() {
     return (
         <div className='App'>
             <h1>Let´s start!!!</h1>
+
+            <Sidebar />
+            {/* Chat */}
         </div>
     );
 }
