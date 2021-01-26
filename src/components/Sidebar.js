fd777637b99dec6900ca1483e446fd6cddaa40cc
AJ-Romera <ajromeraalfonso@gmail.com>
2021-01-26 14:06:34 +0100

    CHNG: Install Material-ui core and icons, Also Use an Icon

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './Sidebar.css';
 
+/* Icons */
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import { Avatar, IconButton } from '@material-ui/core';
+
 function Sidebar() {
     return (
         <div className='sidebar'>
-            <h1>This is the sidebar</h1>
+            <div className='sidebar__header'>
+                <div className='sidebar__headerRight'>
+                    <IconButton>
+                        <DonutLargeIcon />
+                    </IconButton>
+                </div>
+            </div>
         </div>
     );
 }
