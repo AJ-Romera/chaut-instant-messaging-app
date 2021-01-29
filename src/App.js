edf68105df0bfa76b3c54dc0a639a5b80dab0d84
AJ-Romera <ajromeraalfonso@gmail.com>
2021-01-29 12:52:10 +0100

    CHNG: Add Pusher into an useEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import React, { useEffect } from 'react';
 import './App.css';
 import Chat from './components/Chat/Chat';
 import Sidebar from './components/Sidebar/Sidebar';
+import Pusher from 'pusher-js';
 
 function App() {
+    useEffect(() => {
+        const pusher = new Pusher('d8b17a5966dacdd69d8b', {
+            cluster: 'eu',
+        });
+
+        const channel = pusher.subscribe('messages');
+        channel.bind('inserted', (data) => {
+            alert(JSON.stringify(data));
+        });
+    }, []);
+
     return (
         <div className='app'>
             <div className='app__body'>
