acc4dc805ed32a63279b59034c1e6046e4aac072
AJ-Romera <ajromeraalfonso@gmail.com>
2021-01-30 12:24:54 +0100

    CHNG: Add Random Avatars

--- src/components/Sidebar/subComponents/SidebarChat.js
+++ src/components/Sidebar/subComponents/SidebarChat.js
@@ -1,11 +1,19 @@
 import { Avatar } from '@material-ui/core';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './SidebarChat.css';
 
 function SidebarChat() {
+    const [seed, setSeed] = useState('');
+
+    useEffect(() => {
+        setSeed(Math.floor(Math.random() * 5000));
+    }, []);
+
     return (
         <div className='sidebarChat'>
-            <Avatar />
+            <Avatar
+                src={`https://avatars.dicebear.com/api/avataaars/${seed}.svg`}
+            />
             <div className='sidebarChat__info'>
                 <h2>Room name</h2>
                 <p>This is the last message</p>
