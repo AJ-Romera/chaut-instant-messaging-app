08c08f80fb7d21b3b489f23b4f8ef1167ac0bde7
AJ-Romera <ajromeraalfonso@gmail.com>
2021-02-01 16:36:28 +0100

    CHNG: Create StateProvider

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <StateProvider initialState={initialState}>
+            reducer={reducer}
+            <App />
+        </StateProvider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
